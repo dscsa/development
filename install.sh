d08b67c47ee169293d3a583d00340d826775b354
akircher <adam@Adams-MacBook-Pro.local>
2017-05-02 11:39:00 -0700

    Don't automatically start on install

--- install.sh
+++ install.sh
@@ -10,9 +10,7 @@ sudo git clone https://github.com/dscsa/server
 sudo git clone https://github.com/dscsa/pouch
 sudo git clone https://github.com/dscsa/csv
 
-#start the server and client
-cd ../
-ln -s node_modules/development/package.json package.json
-npm start
+#install scripts so npm start, npm run server, npm run client, and npm test all work
+ln -s development/package.json ../package.json
 
 echo test that both http://localhost and http://localhost:9000 now serve the app
