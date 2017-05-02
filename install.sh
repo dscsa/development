41a1ea3f50a84f79e386a203efb5c38379c14bd3
akircher <adam@Adams-MacBook-Pro.local>
2017-05-02 15:18:23 -0700

    Try to allow root running again

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Preinstall script for dscsa/development to setup the proper folder structure
 echo installation almost complete...
 echo ensure couchDB is running on your local computer,
-echo then run 'sudo npm run server' and 'npm run client'
+echo then run \'sudo npm run server\' and \'npm run client\'
 echo sudo is necessary for the server to bind to port 80
 echo test both http://localhost and http://localhost:9000 work
 
@@ -14,5 +14,5 @@ rm -R server && sudo git clone https://github.com/dscsa/server
 rm -R pouch && sudo git clone https://github.com/dscsa/pouch
 rm -R csv && sudo git clone https://github.com/dscsa/csv
 
-#go back to development folder so npm run server, npm run client, and npm test all work
-cd development
+#provide npm run server, npm run client, and npm test to parent directory
+cp development/npm.json ../package.json
