e2edc1996c135ed5a330baa86cbcd2a2fa62d94b
akircher <adam@Adams-MacBook-Pro.local>
2017-05-02 15:44:49 -0700

    Update to only need npm start

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 # Preinstall script for dscsa/development to setup the proper folder structure
 echo installation almost complete...
 echo ensure couchDB is running on your local computer,
-echo then run \'sudo npm run server\' and \'npm run client\'
-echo sudo is necessary for the server to bind to port 80
-echo test both http://localhost and http://localhost:9000 work
+echo run (sudo is necessary for the server to bind to port 80)...
+echo
+echo \'sudo npm start\'
+echo
+echo then test both http://localhost and http://localhost:9000 work
 
 #replace installs with git repos.  Since we are in development we need to leave before deleting
 cd ../
