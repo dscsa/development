b6d7ba1d1c1cd7fe56e33c82ce9c0e4a88492bb2
akircher <adam@Adams-MacBook-Pro.local>
2017-05-02 10:35:48 -0700

    Fix git clone semantics

--- install.sh
+++ install.sh
@@ -2,16 +2,12 @@
 # Preinstall script for dscsa/development to setup the proper folder structure
 echo ensure couchDB is running on your local computer
 
-for entry in ./*
-do
-  echo "$entry"
-done
-
-#replace symlinks with git repos
-rm -R ../client && sudo git clone https://github.com/dscsa/client ../
-rm -R ../server && sudo git clone https://github.com/dscsa/server ../
-rm -R ../pouch && sudo git clone https://github.com/dscsa/pouch ../
-rm -R ../csv && sudo git clone https://github.com/dscsa/csv ../
+#replace installs with git repos
+rm -R ../development && sudo git clone https://github.com/dscsa/development ../development
+rm -R ../client && sudo git clone https://github.com/dscsa/client ../client
+rm -R ../server && sudo git clone https://github.com/dscsa/server ../server
+rm -R ../pouch && sudo git clone https://github.com/dscsa/pouch ../pouch
+rm -R ../csv && sudo git clone https://github.com/dscsa/csv ../csv
 
 #start the server and client
 npm start
