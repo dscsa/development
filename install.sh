7b7bac418cfd4b124ba4f3c493f9053c0b8be44e
akircher <adam@Adams-MacBook-Pro.local>
2017-05-02 11:26:00 -0700

    Remove repo dependency since we were just deleting them anyway

--- install.sh
+++ install.sh
@@ -2,18 +2,17 @@
 # Preinstall script for dscsa/development to setup the proper folder structure
 echo ensure couchDB is running on your local computer
 
-mv node_modules/development/npm.json ../../package.json
-
 #replace installs with git repos
 cd ../
-rm -R development && sudo git clone https://github.com/dscsa/development
-rm -R client && sudo git clone https://github.com/dscsa/client
-rm -R server && sudo git clone https://github.com/dscsa/server
-rm -R pouch && sudo git clone https://github.com/dscsa/pouch
-rm -R csv && sudo git clone https://github.com/dscsa/csv
+sudo git clone https://github.com/dscsa/development
+sudo git clone https://github.com/dscsa/client
+sudo git clone https://github.com/dscsa/server
+sudo git clone https://github.com/dscsa/pouch
+sudo git clone https://github.com/dscsa/csv
 
 #start the server and client
 cd ../
+ln -s node_modules/development/package.json package.json
 npm start
 
 echo test that both http://localhost and http://localhost:9000 now serve the app
