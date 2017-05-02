43f84d64e2fb051d7f5b71fe81150987bfb48f24
akircher <adam@Adams-MacBook-Pro.local>
2017-05-02 00:19:33 -0700

    Remove sudo

--- install.sh
+++ install.sh
@@ -2,16 +2,14 @@
 # Preinstall script for dscsa/development to setup the proper folder structure
 echo ensure couchDB is running on your local computer
 
- #install the development environment
-sudo npm install dscsa/development
-
+cd node_modules
 #replace symlinks with git repos
-sudo rm -R client && sudo git clone https://github.com/dscsa/client
-sudo rm -R server && sudo git clone https://github.com/dscsa/server
-sudo rm -R pouch && sudo git clone https://github.com/dscsa/pouch
-sudo rm -R csv && sudo git clone https://github.com/dscsa/csv
+rm -R client && sudo git clone https://github.com/dscsa/client
+rm -R server && sudo git clone https://github.com/dscsa/server
+rm -R pouch && sudo git clone https://github.com/dscsa/pouch
+rm -R csv && sudo git clone https://github.com/dscsa/csv
 
 #start the server and client
-sudo npm start
+npm start
 
 echo test that both http://localhost and http://localhost:9000 now serve the app
