5f4cb9b209d347769ddaa04f2346c90f19576926
akircher <adam@Adams-MacBook-Pro.local>
2017-05-02 16:50:25 -0700

    Update webdriver-manager automatically on install

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # Preinstall script for dscsa/development to setup the proper folder structure
-echo installation almost complete...
-echo ensure couchDB is running on your local computer,
-echo run - sudo is necessary for the server to bind to port 80...
-echo
-echo \'sudo npm start\'
-echo
-echo then test both http://localhost and http://localhost:9000 work
 
 #replace installs with git repos.  Since we are in development we need to leave before deleting
 cd ../
@@ -18,3 +11,6 @@ rm -R csv && sudo git clone https://github.com/dscsa/csv
 
 #provide npm run server, npm run client, and npm test to parent directory
 cp development/npm.json ../package.json
+
+# downloading protractor doesn't update automatically
+./protractor/node_modules/webdriver-manager/bin/webdriver-manager update
