53e29b54c185308b1d71bdeaee9283a322c4bb50
akircher <adam@Adams-MacBook-Pro-2.local>
2021-02-08 21:43:01 -0500

    remove more optional commands

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ rm -R pouch && sudo git clone https://github.com/dscsa/pouch
 rm -R csv && sudo git clone https://github.com/dscsa/csv
 
 #installing with bash put permission to root only, update to all users
-sudo chmod -R 777 ./node_modules
+# sudo chmod -R 777 ./node_modules
 
 #provide npm run server, npm run client, and npm test to parent directory
 cp development/npm.json ../package.json
 
 # downloading protractor doesn't update automatically
-./protractor/node_modules/webdriver-manager/bin/webdriver-manager update
+# sudo ./protractor/node_modules/webdriver-manager/bin/webdriver-manager update
