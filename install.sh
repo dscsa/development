7fa92ddff201d7f95de31320856e94dee10ef434
akircher <adam@Adams-MacBook-Pro-2.local>
2021-02-08 21:55:56 -0500

    more changes

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 
 #replace installs with git repos.  Since we are in development we need to leave before deleting
 cd ../
-rm -R development && sudo git clone https://github.com/dscsa/development
-rm -R client && sudo git clone https://github.com/dscsa/client
-rm -R server && sudo git clone https://github.com/dscsa/server
-rm -R pouch && sudo git clone https://github.com/dscsa/pouch
-rm -R csv && sudo git clone https://github.com/dscsa/csv
+sudo rm -R development && sudo git clone https://github.com/dscsa/development
+sudo rm -R client && sudo git clone https://github.com/dscsa/client
+sudo rm -R server && sudo git clone https://github.com/dscsa/server
+sudo rm -R pouch && sudo git clone https://github.com/dscsa/pouch
+sudo rm -R csv && sudo git clone https://github.com/dscsa/csv
 
 #installing with bash put permission to root only, update to all users
 # sudo chmod -R 777 ./node_modules
